cd166791478d2ecf63571d3996225c34dc6a87e8
joshmarto <jmartinezolivares1@gmail.com>
2020-07-26 16:23:39 -0600

    Redirect to meeting, instead mean message

--- countdown.js
+++ countdown.js
@@ -19,7 +19,7 @@ simplyCountdown('#cuenta', {
             enableUtc: true, //Use UTC as default
             onEnd: function() {
               //alert('Bienvenidos a nuestra reunion de domingo!');
-              window.location.href = "https://us02web.zoom.us/j/88505585843?pwd=NWFJZzRkMGdINll4VFNzNzZJNjVVQT09";
+              window.location.href = "https://us02web.zoom.us/j/86144981265?pwd=OEJWbTZiM2ZoSHgrS0JqRFBpekVZZz09";
               return; }, //Callback on countdown end, put your own function here
             refresh: 1000, // default refresh every 1s
             sectionClass: 'simply-section', //section css class
