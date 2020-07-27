c49d38b5c5b25a1d93f7bb5b399879917bc5e828
joshmarto <jmartinezolivares1@gmail.com>
2020-07-26 18:52:38 -0600

    Update post meeting

--- countdown.js
+++ countdown.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
             year: 2020, // required
             month: 7, // required
-            day: 26, // required
-            hours: 12, // Default is 0 [0-23] integer. Is our time - 6 hours.
+            day: 30, // required
+            hours: 11, // Default is 0 [0-23] integer. Is our time - 6 hours.
             minutes: 0, // Default is 0 [0-59] integer
             seconds: 0, // Default is 0 [0-59] integer
             words: { //words displayed into the countdown
