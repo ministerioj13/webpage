32bc61ac2dab1911d4e299391ee037c62ea09975
joshmarto <jmartinezolivares1@gmail.com>
2020-07-29 14:22:50 -0600

    Add 'Formulario de Inscripcion'

---
+++ send.js
@@ -0,0 +1,26 @@
+console.log('It works');
+
+$(document).ready(function (){
+  $('.inscription-form').click(function (event){
+    console.log('Clicke button');
+
+    var nombre = $('.name').val();
+    var email = $('.email').val();
+    var comentario = $('.comment').val();
+    var stats = $('.status');
+    stats.empty();
+
+    if (nombre.length > 0){
+      stats.append('<div style="color: green;">Nombre valido</div>');
+    } else{
+      event.preventDefault();
+      stats.append('<div style="color: red;">Por favor ingrese su nombre</div>');
+    }
+    if (email.length > 5 && email.includes('@') && email.includes('.')){
+      stats.append('<div style="color: green;">Email valido</div>');
+    } else{
+      event.preventDefault();
+      stats.append('<div style="color: red;">Email invalido</div>');
+    }
+  })
+})
