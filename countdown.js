1d1f15279af48f90f26327823c78dc11f8346a8a
joshmarto <jmartinezolivares1@gmail.com>
2020-09-26 09:27:04 -0600

    Update 26-9-2020

--- countdown.js
+++ countdown.js
@@ -2,8 +2,8 @@ simplyCountdown('#cuenta', {
             year: 2020, // required
             month: 9, // required
             day: 27, // required
-            hours: 12, // Default is 0 [0-23] integer. Is our time - 6 hours.
-            minutes: 0, // Default is 0 [0-59] integer
+            hours: 11, // Default is 0 [0-23] integer. Is our time - 6 hours.
+            minutes: 45, // Default is 0 [0-59] integer
             seconds: 0, // Default is 0 [0-59] integer
             words: { //words displayed into the countdown
                 days: 'Dia',
