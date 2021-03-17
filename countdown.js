e7fdfe465061b74c13f7dbd63fa6f235770e1f83
joshmarto <jmartinezolivares1@gmail.com>
2021-03-17 08:04:37 -0600

    Update 17-3-2021

--- countdown.js
+++ countdown.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
             year: 2021, // required
             month: 3, // required
-            day: 10, // required
+            day: 17, // required
             hours: 14, // Default is 0 [0-23] integer. Is our time - 6 hours.
             minutes: 0, // Default is 0 [0-59] integer
             seconds: 0, // Default is 0 [0-59] integer
